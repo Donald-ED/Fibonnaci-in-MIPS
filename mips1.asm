9ae01dde6362fc19e78371c449b22bfcf9aa02a8
Donald Echefu <55097668+Donald-ED@users.noreply.github.com>
2022-11-16 09:16:29 -0500

    Update mips1.asm

--- mips1.asm
+++ mips1.asm
@@ -1,6 +1,12 @@
-
+.data
+    prompt: .asciiz "Please enter an integer: "
 .text
 main:
-    li, $v0, 4
-    la, 
+    li $v0, 4
+    la $a0, prompt
+    syscall
+    
+    li $v0, 5
+    syscall
+    move $t0, $v0
 
