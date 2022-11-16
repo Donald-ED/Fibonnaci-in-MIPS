f7a8b69d539bed5537f3d0d09a092712f778f0e5
Donald Echefu <55097668+Donald-ED@users.noreply.github.com>
2022-11-16 09:52:47 -0500

    Update mips1.asm

--- mips1.asm
+++ mips1.asm
@@ -26,6 +26,21 @@ main:
         move $t0, $v0
         
         j while
+    exit:
+        li $v0, 4
+        la $a0, output
+        syscall
+        
+        li $v0, 1
+        move $a0, $t0
+        syscall
+        
+    li $v0, 10
+    syscall
+
+
+
+fib:
     
         
 
